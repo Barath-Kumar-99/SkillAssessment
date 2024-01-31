928847d056e0a499356d94375b69ab1e9389ce67
Barath-Kumar-99 <83857364+Barath-Kumar-99@users.noreply.github.com>
2024-01-31 20:28:48 +0530

    Update ConversionResponse.go

--- customerLabs/dto/ConversionResponse.go
+++ customerLabs/dto/ConversionResponse.go
@@ -24,38 +24,3 @@ type Trait struct {
 	Type  string `json:"type"`
 }
 
-//type ConversionResponseDto struct {
-//	Event           string `json:"event"`
-//	EventType       string `json:"event_type"`
-//	AppId           string `json:"app_id"`
-//	UserId          string `json:"user_id"`
-//	MessageId       string `json:"message_id"`
-//	PageTitle       string `json:"page_title"`
-//	PageUrl         string `json:"page_url"`
-//	BrowserLanguage string `json:"browser_language"`
-//	ScreenSize      string `json:"screen_size"`
-//	Attributes      struct {
-//		FormVarient struct {
-//			Value string `json:"value"`
-//			Type  string `json:"type"`
-//		} `json:"form_varient"`
-//		Ref struct {
-//			Value string `json:"value"`
-//			Type  string `json:"type"`
-//		} `json:"ref"`
-//	} `json:"attributes"`
-//	Traits struct {
-//		Age struct {
-//			Value string `json:"value"`
-//			Type  string `json:"type"`
-//		} `json:"age"`
-//		Email struct {
-//			Value string `json:"value"`
-//			Type  string `json:"type"`
-//		} `json:"email"`
-//		Name struct {
-//			Value string `json:"value"`
-//			Type  string `json:"type"`
-//		} `json:"name"`
-//	} `json:"traits"`
-//}
